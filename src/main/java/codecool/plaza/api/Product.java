ab61b4b4816b380d19ec6fc8f84a68dd06cf54e1
mtganyu <mtganyu@gmail.com>
2018-02-16 08:50:08 +0100

    fixed serialization

--- src/main/java/codecool/plaza/api/Product.java
+++ src/main/java/codecool/plaza/api/Product.java
@@ -2,7 +2,7 @@ package codecool.plaza.api;
 
 import java.io.*;
 
-public abstract class Product {
+public abstract class Product implements Serializable {
 
     String name;
     long barcode;
@@ -29,36 +29,6 @@ public abstract class Product {
     public String toString() {
         return "";
     }
-    public void serializeProd() {
-        try {
-            FileOutputStream fileOut = new FileOutputStream("./src/main/java/codecool/plaza/data/productser.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(this);
-            out.close();
-            fileOut.close();
-            System.out.println("Serialized data is saved in productser.ser");
-        } catch (IOException i) {
-            i.printStackTrace();
-        }
-    }
 
-  /*  public static Product deserializeProd() {
-        try {
-            FileInputStream fileIn = new FileInputStream("./src/main/java/codecool/plaza/data/productser.ser");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            Product p = (Product) in.readObject();
-            in.close();
-            fileIn.close();
-            return p;
-
-        } catch (IOException i) {
-            i.printStackTrace();
-
-        } catch (ClassNotFoundException c) {
-            System.out.println("product class not found");
-            c.printStackTrace();
-        }
-        return null;
-    }*/
 
 }
